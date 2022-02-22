348871945dcf20536e7023310f17b3d6b1c2522d
pool2win <pool2win@users.noreply.github.com>
2022-02-22 09:29:17 +0100

    Clean up and move tests around

--- bitcoin/sighash.rkt
+++ bitcoin/sighash.rkt
@@ -18,10 +18,20 @@
     (bytes-append (hex->bytes (outpoint-transaction-hash point))
                   (integer->integer-bytes (outpoint-index point) index-size signed big-endian))))
 
+(module+ test
+  (test-case
+      "digest outpoint"
+    (check-equal? (digest-outpoint (outpoint "deadbeef" 1))
+                  (bytes-append (hex->bytes "deadbeef")
+                                (integer->integer-bytes 1 4 #f #f)))))
 
 (define (digest-transaction-inputs tx index [sighash 'all])
   (bytes-append* (for/list ([input (transaction-inputs tx)])
                    (digest-outpoint (input-prevout input)))))
+
+(define (digest-input-sequences tx)
+  (bytes-append* (for/list ([input (transaction-inputs tx)])
+                   (to-little-endian-4-bytes (input-sequence input)))))
 
 (module+ test
     (test-case
@@ -38,10 +48,6 @@
                       (bytes-append (integer->integer-bytes 1234 4 #f #f)
                                     (integer->integer-bytes 5678 4 #f #f))))))
 
-(define (digest-input-sequences tx)
-  (bytes-append* (for/list ([input (transaction-inputs tx)])
-                   (to-little-endian-4-bytes (input-sequence input)))))
-
 ;; Ignore sighash for now, assume sighash all
 ;; https://github.com/bitcoin/bips/blob/master/bip-0143.mediawiki
 (define (transaction-digest-for-sign tx index [sighash 'all])
@@ -52,20 +58,3 @@
                 (transaction-lock-time tx)
                 (transaction-inputs tx)
                 (transaction-outputs tx)))
-
-(module+ test
-  (test-case
-      "digest outpoint"
-    (check-equal? (digest-outpoint (outpoint "deadbeef" 1))
-                  (bytes-append (hex->bytes "deadbeef")
-                                (integer->integer-bytes 1 4 #f #f))))
-    (test-case
-        "digest prevouts for transaction inputs"
-      (let* ([input1 (input '() '() 1234 (outpoint "deadbeef" 1))]
-             [input2 (input '() '() 1234 (outpoint "deadbeff" 2))]
-             )
-        (check-equal? (digest-transaction-inputs (transaction 1 1 (list input1 input2) '() '() 1) 1)
-                      (bytes-append (hex->bytes "deadbeef")
-                                    (integer->integer-bytes 1 4 #f #f)
-                                    (hex->bytes "deadbeff")
-                                    (integer->integer-bytes 2 4 #f #f))))))
