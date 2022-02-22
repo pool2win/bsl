24430ffe119c5d617cb0a3fa4c881c16da5d9308
pool2win <pool2win@users.noreply.github.com>
2022-02-22 10:14:52 +0100

    Simplify function names

--- bitcoin/sighash.rkt
+++ bitcoin/sighash.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(provide digest-transaction-inputs transaction-digest-for-sign)
+(provide transaction-digest-for-sign)
 
 (module+ test
   (require rackunit))
@@ -25,11 +25,11 @@
                   (bytes-append (hex->bytes "deadbeef")
                                 (integer->integer-bytes 1 4 #f #f)))))
 
-(define (digest-transaction-inputs tx index [sighash 'all])
+(define (digest-prevouts tx index [sighash 'all])
   (bytes-append* (for/list ([input (transaction-inputs tx)])
                    (digest-outpoint (input-prevout input)))))
 
-(define (digest-input-sequences tx)
+(define (digest-sequences tx)
   (bytes-append* (for/list ([input (transaction-inputs tx)])
                    (to-little-endian-4-bytes (input-sequence input)))))
 
@@ -39,12 +39,12 @@
       (let* ([input1 (input '() '() 1234 (outpoint "deadbeef" 1))]
              [input2 (input '() '() 5678 (outpoint "deadbeff" 2))]
              [test-tx (transaction 1 1 (list input1 input2) '() '() 1)])
-        (check-equal? (digest-transaction-inputs test-tx 1)
+        (check-equal? (digest-prevouts test-tx 1)
                       (bytes-append (hex->bytes "deadbeef")
                                     (integer->integer-bytes 1 4 #f #f)
                                     (hex->bytes "deadbeff")
                                     (integer->integer-bytes 2 4 #f #f)))
-        (check-equal? (digest-input-sequences test-tx)
+        (check-equal? (digest-sequences test-tx)
                       (bytes-append (integer->integer-bytes 1234 4 #f #f)
                                     (integer->integer-bytes 5678 4 #f #f))))))
 
@@ -52,8 +52,8 @@
 ;; https://github.com/bitcoin/bips/blob/master/bip-0143.mediawiki
 (define (transaction-digest-for-sign tx index [sighash 'all])
   (bytes-append (to-little-endian-4-bytes (transaction-version-number tx))
-                (double-sha256-hash (digest-transaction-inputs tx index))
-                (double-sha256-hash (digest-input-sequences tx))
+                (double-sha256-hash (digest-prevouts tx index))
+                (double-sha256-hash (digest-sequences tx))
                 (digest-outpoint (list-ref transaction-inputs index))
                 (transaction-lock-time tx)
                 (transaction-inputs tx)
