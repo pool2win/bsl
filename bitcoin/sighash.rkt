ce5335281f3ce32660134ce1825effdd378b520b
pool2win <pool2win@users.noreply.github.com>
2022-03-19 20:19:58 +0100

    Add support for low hanging signature hash parts

--- bitcoin/sighash.rkt
+++ bitcoin/sighash.rkt
@@ -65,7 +65,17 @@
 (define (serialize-amount amount)
   (to-little-endian-n-bytes amount 8))
 
-(define (serialize-script-code tx ix)
+(define (serialize-input-sequence tx index)
+  (to-little-endian-n-bytes (input-sequence (list-ref (transaction-inputs tx) index)) 4))
+
+(define (serialize-transaction-lock-time tx)
+  (to-little-endian-n-bytes (transaction-lock-time tx) 4))
+
+(define (serialize-sighash-type sighash)
+  (to-little-endian-n-bytes sighash 4))
+
+;; TODO - use codeseparator to 
+(define (serialize-script-code tx index sighash)
   '())
 
 (define sighash-all  #x00000001)
@@ -80,11 +90,11 @@
                 (serialize-prevouts tx index)
                 (serialize-sequences tx)
                 (serialize-outpoint tx index)
-                (serialize-script-code tx)
-                (serialize-amount amount)
-                (transaction-lock-time tx) ;; TODO
-                (transaction-inputs tx) ;; TODO
-                (transaction-outputs tx) ;; TODO
+                (serialize-script-code tx index sighash)
+                (serialize-amount amount) ;; often not known, and is therefore provided separately
+                (serialize-input-sequence tx index)
+                (serialize-transaction-lock-time tx)
+                (serialize-sighash-type tx)
                 ))
 
 (module+ test
@@ -99,10 +109,15 @@
                                              #:inputs test-inputs
                                              #:outputs test-outputs
                                              #:witnesses '()
-                                             #:lock-time '())])
+                                             #:lock-time 500001)])
     (check-equal? (serialize-version-number test-transaction) (to-little-endian-n-bytes 1 4))
     (check-equal? (serialize-prevouts test-transaction 0) (double-sha256-hash (digest-prevouts test-transaction 0)))
     (check-equal? (serialize-sequences test-transaction) (double-sha256-hash (digest-sequences test-transaction)))
     (check-equal? (serialize-outpoint test-transaction 0) (digest-outpoint test-prevout))
+    ;; (check-equal? (serialize-script-code test-transaction 0)
+    ;;               (digest-outpoint (input-script test-prevout))) ;; TODO correct this
     (check-equal? (serialize-amount 100) (to-little-endian-n-bytes 100 8))
+    (check-equal? (serialize-input-sequence test-transaction 0) (to-little-endian-n-bytes 1 4))
+    (check-equal? (serialize-transaction-lock-time test-transaction) (to-little-endian-n-bytes 500001 4))
+    (check-equal? (serialize-sighash-type sighash-all) (to-little-endian-n-bytes #x00000001 4))
     )))
