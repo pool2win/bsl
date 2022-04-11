41177b59f51835c66b319c9437336039a929e6c1
pool2win <pool2win@users.noreply.github.com>
2022-04-11 10:19:53 +0200

    Move all supported opcodes to lambda
    The lambda takes script and stack args, returns script and stack

--- bitcoin/script-processor/bitcoin-environment.rkt
+++ bitcoin/script-processor/bitcoin-environment.rkt
@@ -12,46 +12,37 @@
 
 (define (make-bitcoin-environment)
   (let ([env (make-initial-env)])
-    (add-opcode env '(op_0 op_false) #x00 (lambda (script stack)
-                                            (values script (cons 0 stack))))
+    (add-opcode env '(op_0 op_false) #x00
+                (lambda (script stack)
+                  (values script (cons 0 stack))))
     (for ([code (in-inclusive-range 1 75)])
       (add-opcode env `("op_",(~a code)) code
                   (lambda (script stack)
                     (values (list-tail script 1) (cons (first script) stack)))))
+    (add-opcode env '(op_pushdata1) #x4c
+                (lambda (script stack)
+                  (values (list-tail script 2) (cons (second script) stack))))
+    (add-opcode env '(op_pushdata2) #x4d
+                (lambda (script stack)
+                  (values (list-tail script 2) (cons (second script) stack))))
+    (add-opcode env '(op_pushdata4) #x4e
+                (lambda (script stack)
+                  (values (list-tail script 2) (cons (second script) stack))))
+    (add-opcode env '(op_1negate) #x4f
+                (lambda (script stack)
+                  (values script  (cons -1 stack))))
+    (add-opcode env '(op_1 op_true) #x51
+                (lambda (script stack)
+                  (values script (cons 1 stack))))
+    (for ([code (in-inclusive-range #x52 #x60)])
+      (add-opcode env (string-join `("op_",(~a (- code 80))) "") code
+                  (lambda (script stack)
+                    (values script (cons (- code 80) stack)))))
+    (add-opcode env '(op_nop) #x61
+                (lambda (script stack)
+                  (values script stack)))
     env))
-                              
-    ;; (add-opcode '(op_pushdata1) #x4c
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 3
-    ;;              #:proc (lambda (code num-bytes bytes-to-push)
-    ;;                       bytes-to-push)) env)
-    ;; (add-opcode '(op_pushdata2) #x4d
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 3
-    ;;              #:proc (lambda (code num-bytes bytes-to-push)
-    ;;                       bytes-to-push)) env)
-    ;; (add-opcode '(op_pushdata4) #x4e
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 3
-    ;;              #:proc (lambda (code num-bytes bytes-to-push)
-    ;;                       bytes-to-push)) env)
-    ;; (add-opcode '(op_1negate) #x4f
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 1
-    ;;              #:proc (lambda (code) -1)) env)
-    ;; (add-opcode '(op_1 op_true) #x51
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 1
-    ;;              #:proc (lambda (code) 1)) env)
-    ;; (for ([code (in-inclusive-range #x52 #x60)])
-    ;;   (add-opcode (string-join `("op_",(~a (- code 80))) "") code
-    ;;               (make-opcode
-    ;;                #:num-arguments 0 #:push-to-stack #t #:pop-from-stack 0 #:read-ahead-from-script 1
-    ;;                #:proc (lambda (code) (- code 80))) env))
-    ;; (add-opcode '(op_nop) #x61
-    ;;             (make-opcode
-    ;;              #:num-arguments 0 #:push-to-stack #f #:pop-from-stack 0 #:read-ahead-from-script 1
-    ;;              #:proc (lambda (code) '())) env)
+                                  
     ;; (add-opcode '(op_if) #x63
     ;;             (make-opcode
     ;;              #:num-arguments 0 #:push-to-stack #f #:pop-from-stack 0 #:read-ahead-from-script 0
