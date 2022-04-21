1f06292bb32b9af9b4ee3f3e8636a4809518379c
pool2win <pool2win@users.noreply.github.com>
2022-04-22 00:05:44 +0200

    use in-port instead of byte-stream

--- bitcoin/transaction-reader.rkt
+++ bitcoin/transaction-reader.rkt
@@ -2,7 +2,6 @@
 
 (require file/sha1
          racket/stream
-         "byte-reader.rkt"
          "transaction.rkt"
          "endian-helper.rkt"
          "varint-reader.rkt")
@@ -12,41 +11,34 @@
 (module+ test
   (require rackunit))
 
-(define (parse-version tx-data-stream)
-  (let-values ([(version-bytes rest) (next-n-bytes-and-rest tx-data-stream 4)])
-    (values (read-little-endian-bytes version-bytes) rest)))
+(define (parse-version tx-port)
+  (read-little-endian-bytes (read-bytes 4 tx-port)))
 
-(define (parse-hash tx-data-stream)
-  (let-values ([(hash-bytes rest) (next-n-bytes-and-rest tx-data-stream 32)])
-    (values hash-bytes rest)))
+(define (parse-hash tx-port)
+  (read-bytes 32 tx-port))
 
-(define (parse-point tx-data-stream)
-  (let*-values ([(tx-hash rest) (parse-hash tx-data-stream)]
-                [(index-bytes rest) (next-n-bytes-and-rest rest 4)]
-                [(index) (read-little-endian-bytes index-bytes)])
-    (values (outpoint tx-hash index) rest)))
+(define (parse-point tx-port)
+  (let* ([tx-hash (parse-hash tx-port)]
+         [index (read-little-endian-bytes (read-bytes 4 tx-port))])
+    (outpoint tx-hash index)))
 
-(define (parse-script tx-data-stream)
-  (let*-values ([(script-size tx-data-stream) (read-varint-value tx-data-stream)]
-                [(script-bytes tx-data-stream) (next-n-bytes-and-rest tx-data-stream script-size)])
-    (values script-bytes tx-data-stream)))
+(define (parse-script tx-port)
+  (read-bytes (read-varint-value tx-port) tx-port))
 
-(define (parse-sequence tx-data-stream)
-  (let*-values ([(sequence-bytes rest) (next-n-bytes-and-rest tx-data-stream 4)]
-                [(sequence) (read-little-endian-bytes sequence-bytes)])
-    (values sequence rest)))
+(define (parse-sequence tx-port)
+  (read-little-endian-bytes (read-bytes 4 tx-port)))
 
-(define (parse-input tx-data-stream)
-  (let*-values ([(point rest) (parse-point tx-data-stream)]
-                [(script rest) (parse-script rest)]
-                [(sequence rest) (parse-sequence rest)])
-    (values (make-input #:script script #:witness '() #:sequence sequence #:point point) rest)))
+(define (parse-input tx-port)
+  (let* ([point (parse-point tx-port)]
+         [script (parse-script tx-port)]
+         [sequence (parse-sequence tx-port)])
+    (make-input #:script script #:witness '() #:sequence sequence #:point point)))
 
-(define (parse-inputs num-inputs inputs tx-data-stream)
+(define (parse-inputs num-inputs inputs tx-port)
   (cond
-    [(= 0 num-inputs) (values inputs tx-data-stream)]
-    [else (let-values ([(input tx-data-stream) (parse-input tx-data-stream)])
-            (parse-inputs (sub1 num-inputs) (append inputs (list input)) tx-data-stream))]))
+    [(= 0 num-inputs) inputs]
+    [else (let ([input (parse-input tx-port)])
+            (parse-inputs (sub1 num-inputs) (append inputs (list input)) tx-port))]))
 
 (define (parse-outputs tx-data-stream)
   (values '() tx-data-stream))
@@ -57,31 +49,32 @@
 (define (decode-transaction tx-data)
   ;; TODO: Use tx-data as a stream, so there is no need to track the 'byte locations' as we process the data
   ;; Explore using stream-take and for/stream etc functions
-  (let*-values ([(tx-data-stream) (in-bytes tx-data)]
-                [(version-number tx-data-stream) (parse-version tx-data-stream)]
-                [(num-inputs tx-data-stream) (read-varint-value tx-data-stream)]
-                [(inputs tx-data-stream) (parse-inputs num-inputs '() tx-data-stream)]
-                [(outputs tx-data-stream) (parse-outputs tx-data-stream)]
-                [(locktime tx-data-stream) (parse-locktime tx-data-stream)])
+  (let* ([tx-port (open-input-bytes tx-data)]
+         [version-number (parse-version tx-port)]
+         [num-inputs (read-varint-value tx-port)]
+         [inputs (parse-inputs num-inputs '() tx-port)]
+         [outputs (parse-outputs tx-port)]
+         [locktime (parse-locktime tx-port)])
     (transaction version-number inputs outputs locktime)))
 
 (module+ test
   (test-case
       "parse transaction from bytes"
-    (let*-values ([(tx-data) "907c2bc503ade11cc3b04eb2918b6f547b0630ab569273824748c87ea14b0696526c66ba740200000004ab65ababfd1f9bdd4ef073c7afc4ae00da8a66f429c917a0081ad1e1dabce28d373eab81d8628de802000000096aab5253ab52000052ad042b5f25efb33beec9f3364e8a9139e8439d9d7e26529c3c30b6c3fd89f8684cfd68ea0200000009ab53526500636a52ab599ac2fe02a526ed040000000008535300516352515164370e010000000003006300ab2ec229"]
-           [(tx-bytes) (hex-string->bytes tx-data)]
-           [(tx-data-stream) (sequence->stream (in-bytes tx-bytes))]
-           [(version-number tx-data-stream) (parse-version tx-data-stream)]
-           [(num-inputs tx-data-stream) (read-varint-value tx-data-stream)]
-           [(inputs tx-data-stream) (parse-inputs num-inputs '() tx-data-stream)])
+    (let* ([tx-data "907c2bc503ade11cc3b04eb2918b6f547b0630ab569273824748c87ea14b0696526c66ba740200000004ab65ababfd1f9bdd4ef073c7afc4ae00da8a66f429c917a0081ad1e1dabce28d373eab81d8628de802000000096aab5253ab52000052ad042b5f25efb33beec9f3364e8a9139e8439d9d7e26529c3c30b6c3fd89f8684cfd68ea0200000009ab53526500636a52ab599ac2fe02a526ed040000000008535300516352515164370e010000000003006300ab2ec229"]
+           [tx-bytes (hex-string->bytes tx-data)]
+           [tx-port (open-input-bytes tx-bytes)]
+           [version-number (parse-version tx-port)]
+           [num-inputs (read-varint-value tx-port)]
+           [inputs (parse-inputs num-inputs '() tx-port)])
       (check-equal? version-number 3307961488)
       (check-equal? num-inputs 3)
-      (check-equal? inputs (list
-                            (input '() '() 3717931005
-                                   (outpoint
-                                    #"\255\341\34\303\260N\262\221\213oT{\0060\253V\222s\202GH\310~\241K\6\226Rlf\272t" 2))
-                            (input '() '() 1596654765
-                                   (outpoint
-                                    #"N\360s\307\257\304\256\0\332\212f\364)\311\27\240\b\32\321\341\332\274\342\2157>\253\201\330b\215\350" 2))
-                            (input '() '() 4274166361
-                                   (outpoint #"%\357\263;\356\311\3636N\212\2219\350C\235\235~&R\234<0\266\303\375\211\370hL\375h\352" 2)))))))
+      (check-equal? inputs
+                    (list
+                     (input #"\253e\253\253" '() 3717931005
+                            (outpoint
+                             #"\255\341\34\303\260N\262\221\213oT{\0060\253V\222s\202GH\310~\241K\6\226Rlf\272t" 2))
+                     (input #"j\253RS\253R\0\0R" '() 1596654765
+                            (outpoint
+                             #"N\360s\307\257\304\256\0\332\212f\364)\311\27\240\b\32\321\341\332\274\342\2157>\253\201\330b\215\350" 2))
+                     (input #"\253SRe\0cjR\253" '() 4274166361
+                            (outpoint #"%\357\263;\356\311\3636N\212\2219\350C\235\235~&R\234<0\266\303\375\211\370hL\375h\352" 2)))))))
--- bitcoin/varint-reader.rkt
+++ bitcoin/varint-reader.rkt
@@ -1,8 +1,7 @@
-#lang racket/base
+#lang errortrace racket/base
 
 (require file/sha1
          racket/stream
-         "byte-reader.rkt"
          "endian-helper.rkt")
 
 (provide read-varint-value)
@@ -11,36 +10,32 @@
   (require rackunit))
 
 
-(define (read-varint-value from-stream)
-  (let*-values ([(varint-2-bytes-flag) (integer->integer-bytes #xfd 1 #f)]
-                [(varint-4-bytes-flag) (integer->integer-bytes #xfe 1 #f)]
-                [(varint-8-bytes-flag) (integer->integer-bytes #xff 1 #f)]
-                [(first-byte rest) (next-n-bytes-and-rest from-stream 1)]
-                [(value rest) (cond
-                                [(equal? first-byte varint-8-bytes-flag)
-                                 (next-n-bytes-and-rest rest 8)]
-                                [(equal? first-byte varint-4-bytes-flag)
-                                 (next-n-bytes-and-rest rest 4)]
-                                [(equal? first-byte varint-2-bytes-flag)
-                                 (next-n-bytes-and-rest rest 2)]
-                                [else
-                                 (values first-byte rest)])])
-    (values (read-little-endian-bytes value) rest)))
+(define (read-varint-value in)
+  (let* ([varint-2-bytes-flag (integer->integer-bytes #xfd 1 #f)]
+         [varint-4-bytes-flag (integer->integer-bytes #xfe 1 #f)]
+         [varint-8-bytes-flag (integer->integer-bytes #xff 1 #f)]
+         [first-byte (read-bytes 1 in)]
+         [value (cond
+                  [(equal? first-byte varint-8-bytes-flag)
+                   (read-bytes 8 in)]
+                  [(equal? first-byte varint-4-bytes-flag)
+                   (read-bytes 4 in)]
+                  [(equal? first-byte varint-2-bytes-flag)
+                   (read-bytes 2 in)]
+                  [else first-byte])])
+    (read-little-endian-bytes value)))
 
 
 (module+ test
   (test-case
       "read a varint from given hex encoded bytes"
-    (let-values ([(value rest) (read-varint-value (sequence->stream (in-bytes (hex-string->bytes "bb"))))]) 
+    (let ([value (read-varint-value (open-input-bytes (hex-string->bytes "bb")))]) 
       (check-equal?  value 187))
-    (let-values ([(value rest) (read-varint-value (sequence->stream (in-bytes (hex-string->bytes "fd0100"))))]) 
+    (let ([value (read-varint-value (open-input-bytes (hex-string->bytes "fd0100")))])
       (check-equal?  value 1))
-    (let-values ([(value rest) (read-varint-value (sequence->stream (in-bytes (hex-string->bytes "fe01000000"))))]) 
+    (let ([value (read-varint-value (open-input-bytes (hex-string->bytes "fe01000000")))])
       (check-equal?  value 1))
-    (let-values ([(value rest) (read-varint-value
-                                (sequence->stream (in-bytes (hex-string->bytes "ff0100000000000000"))))])
+    (let ([value (read-varint-value (open-input-bytes (hex-string->bytes "ff0100000000000000")))])
       (check-equal?  value 1))
-    (let-values ([(value rest) (read-varint-value (sequence->stream (in-bytes (hex-string->bytes "fd0100abab"))))])
-      (check-equal?  value 1)
-      (check-equal?  (stream-length rest) 2)
-      (check-equal?  (bytes->hex-string (list->bytes (stream->list rest))) "abab"))))
+    (let ([value (read-varint-value (open-input-bytes (hex-string->bytes "fd0100abab")))])
+      (check-equal?  value 1))))
