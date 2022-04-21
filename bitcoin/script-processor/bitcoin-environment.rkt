99ebf6ac2a3aa948cf5cefd850ec7498ed04eddc
pool2win <pool2win@users.noreply.github.com>
2022-04-21 10:49:33 +0200

    Add support for altstack

--- bitcoin/script-processor/bitcoin-environment.rkt
+++ bitcoin/script-processor/bitcoin-environment.rkt
@@ -12,73 +12,77 @@
 (module+ test
   (require rackunit))
 
-(define (handle-conditional condition env script stack)
+(define (handle-conditional condition env script stack altstack)
   (match script
     [(list ifexp endifexp)
      #:when (and (eq? endifexp 'op_endif) (eq? (first stack) condition))
-     (let-values ([(rest-script new-env new-stack) (eval-script ifexp env (rest stack))])
-       (values (list-tail script 2) new-stack #t))]
+     (let-values ([(rest-script new-env new-stack) (eval-script ifexp env (rest stack) altstack)])
+       (values (list-tail script 2) new-stack altstack #t))]
     [(list ifexp endifexp)
      #:when (and (eq? endifexp 'op_endif) (not (eq? (first stack) condition)))
-       (values (list-tail script 2) (rest stack) #t)]
+       (values (list-tail script 2) (rest stack) altstack #t)]
     [(list ifexp elseexp elseifexp endifexp)
      #:when (and (eq? elseexp 'op_else) (eq? endifexp 'op_endif) (eq? (first stack) condition))
-     (let-values ([(rest-script new-env stack) (eval-script ifexp env (rest stack))])
-       (values (list-tail script 4) stack #t))]
+     (let-values ([(rest-script new-env stack) (eval-script ifexp env (rest stack) altstack)])
+       (values (list-tail script 4) stack altstack #t))]
     [(list ifexp elseexp elseifexp endifexp)
      #:when (and (eq? elseexp 'op_else) (eq? endifexp 'op_endif) (not (eq? (first stack) condition)))
-     (let-values ([(rest-script new-env stack) (eval-script elseifexp env (rest stack))])
-       (values (list-tail script 4) stack #t))]))
+     (let-values ([(rest-script new-env stack) (eval-script elseifexp env (rest stack) altstack)])
+       (values (list-tail script 4) stack altstack #t))]))
 
 (define (make-bitcoin-environment)
   (let ([env (make-initial-env)])
     (add-opcode env '(op_0 op_false) #x00
-                (lambda (script stack)
-                  (values script (cons 0 stack) #t)))
+                (lambda (script stack altstack)
+                  (values script (cons 0 stack) altstack #t)))
     (for ([code (in-inclusive-range 1 75)])
       (add-opcode env (string-join `("op_" ,(~a code)) "") code
-                  (lambda (script stack)
-                    (values (list-tail script 1) (cons (first script) stack) #t))))
+                  (lambda (script stack altstack)
+                    (values (list-tail script 1) (cons (first script) stack) altstack #t))))
     (add-opcode env '(op_pushdata1) #x4c
-                (lambda (script stack)
-                  (values (list-tail script 2) (cons (second script) stack) #t)))
+                (lambda (script stack altstack)
+                  (values (list-tail script 2) (cons (second script) stack) altstack #t)))
     (add-opcode env '(op_pushdata2) #x4d
-                (lambda (script stack)
-                  (values (list-tail script 2) (cons (second script) stack) #t)))
+                (lambda (script stack altstack)
+                  (values (list-tail script 2) (cons (second script) stack) altstack #t)))
     (add-opcode env '(op_pushdata4) #x4e
-                (lambda (script stack)
-                  (values (list-tail script 2) (cons (second script) stack) #t)))
+                (lambda (script stack altstack)
+                  (values (list-tail script 2) (cons (second script) stack) altstack #t)))
     (add-opcode env '(op_1negate) #x4f
-                (lambda (script stack)
-                  (values script  (cons -1 stack) #t)))
+                (lambda (script stack altstack)
+                  (values script  (cons -1 stack) altstack #t)))
     (add-opcode env '(op_1 op_true) #x51
-                (lambda (script stack)
-                  (values script (cons 1 stack) #t)))
+                (lambda (script stack altstack)
+                  (values script (cons 1 stack) altstack #t)))
     (for ([code (in-inclusive-range #x52 #x60)])
       (add-opcode env (string-join `("op_" ,(~a (- code 80))) "") code
-                  (lambda (script stack)
-                    (values script (cons (- code 80) stack) #t))))
+                  (lambda (script stack altstack)
+                    (values script (cons (- code 80) stack) altstack #t))))
     (add-opcode env '(op_nop) #x61
-                (lambda (script stack)
-                  (values script stack #t)))
+                (lambda (script stack altstack)
+                  (values script stack altstack #t)))
     (add-opcode env '(op_if) #x63
-                (lambda (script stack)
-                  (handle-conditional 1 env script stack)))
+                (lambda (script stack altstack)
+                  (handle-conditional 1 env script stack altstack )))
     (add-opcode env '(op_notif) #x64
-                (lambda (script stack)
-                  (handle-conditional 0 env script stack)))
+                (lambda (script stack altstack)
+                  (handle-conditional 0 env script stack altstack)))
     (add-opcode env '(op_else) #x67
-                (lambda (script stack)
-                  (values script stack #t)))
+                (lambda (script stack altstack)
+                  (values script stack altstack #t)))
     (add-opcode env '(op_endif) #x68
-                (lambda (script stack)
-                  (values script stack #t)))
+                (lambda (script stack altstack)
+                  (values script stack altstack #t)))
     (add-opcode env '(op_verify) #x69
-                (lambda (script stack)
-                  (values script stack (eq? (first stack) 1))))
+                (lambda (script stack altstack)
+                  (values script stack altstack (eq? (first stack) 1))))
     (add-opcode env '(op_return) #x6a
-                (lambda (script stack)
-                  (values '() stack #f)))
+                (lambda (script stack altstack)
+                  (values '() stack altstack #f)))
+    ;; ;; stack operations
+    ;; (add-opcode env '(op_toaltstack) #x6b
+    ;;             (lambda (script stack altstack)
+    ;;               (values '() stack () altstack #f)))
     env))
                                   
 (module+ test
