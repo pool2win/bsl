f967b27422cad8e69f042cfec6c2e675dbcd33ec
pool2win <pool2win@users.noreply.github.com>
2022-05-10 10:13:37 +0200

    Setup ground work for tx_valid tests

---
+++ bitcoin/script-processor/test-tx-valid-suite.rkt
@@ -0,0 +1,23 @@
+#lang errortrace racket/base
+
+(require "environment.rkt"
+         "bitcoin-environment.rkt"
+         "eval-script.rkt"
+         "../transaction.rkt"
+         "../transaction-reader.rkt"
+         "../../crypto-utils.rkt")
+
+(module+ test
+  (require rackunit))
+
+
+(module+ test
+  (test-case
+      "run tx valid cases"
+    (let ([bitcoin-env (make-bitcoin-environment)]
+          [tx (decode-transaction "010000000100010000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000")])
+      (check-equal? #"\0\1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+                    (outpoint-transaction-hash (input-point (list-ref (transaction-inputs tx) 0))))
+      (check-equal? 0 (outpoint-index (input-point (list-ref (transaction-inputs tx) 0))))
+      (check-equal? #"" (input-script (list-ref (transaction-inputs tx) 0)))
+      )))
