6d84abd73f7141f63a579f4daad1006b4875f0fe
pool2win <pool2win@users.noreply.github.com>
2023-11-24 15:19:26 +0100

    Reorganise transaction-lang module and tests

--- bitcoin/transaction-lang.rkt
+++ bitcoin/transaction-lang.rkt
@@ -4,8 +4,7 @@
 (require "transaction.rkt"
          "endian-helper.rkt")
 
-(module+ test
-  (require rackunit))
+(provide (all-defined-out))
 
 (define default-version-number 2) ;; we only work with version 2 transactions and up
 (define default-flag 1)
@@ -38,7 +37,7 @@
                              [v (syntax->datum #'(a ...))])
                     (output s v)))))
 
-(define-syntax (transaction stx)    
+(define-syntax (tx stx)    
   (syntax-parse stx
     [(_
       (~alt
@@ -59,69 +58,3 @@
                              #:inputs i
                              #:outputs o))]))
   
-(module+ test
-  (test-case "parse transactions from the new lang")
-  (check-equal? (transaction-version-number (transaction)) default-version-number)
-  (check-equal? (transaction-flag (transaction)) default-flag)
-  (check-equal? (transaction-lock-time (transaction)) default-lock-time)
-  (check-equal? (transaction-version-number (transaction #:version-number 100)) 100)
-
-  (check-equal? (transaction-flag (transaction #:flag "f")) "f")
-  (check-equal? (transaction-version-number (transaction #:version-number 10 #:flag "g")) 10)
-  (check-equal? (transaction-flag (transaction #:version-number 10 #:flag "g")) "g")
-
-  (let ([tx (transaction #:lock-time 10
-                         #:version-number 10
-                         #:flag "g"
-                         #:inputs ((#:sequence "FFFFFFFF" #:prevout ("abcd" 1) #:witness "w1" #:script "pkh(alice)")
-                                   (#:sequence "FFFFFFFF" #:witness "w2" #:prevout ("hash2" 2) #:script "op2")))]) 
-    (check-equal? (transaction-flag tx) "g")
-    (check-equal? (transaction-lock-time tx) 10)
-    (check-equal? (transaction-version-number  tx) 10)
-    (check-equal? (length (transaction-inputs tx)) 2)
-    (check-equal? (input-script (list-ref (transaction-inputs tx) 0)) "pkh(alice)")
-    (check-equal? (input-witness (list-ref (transaction-inputs tx) 0)) "w1")
-    (check-equal? (input-prevout (list-ref (transaction-inputs tx) 0)) (outpoint "abcd" 1))
-    (check-equal? (input-sequence (list-ref (transaction-inputs tx) 0)) (read-little-endian-hex-string "FFFFFFFF"))
-    
-    (check-equal? (input-script (list-ref (transaction-inputs tx) 1)) "op2")
-    (check-equal? (input-witness (list-ref (transaction-inputs tx) 1)) "w2")
-    (check-equal? (input-prevout (list-ref (transaction-inputs tx) 1)) (outpoint "hash2" 2))
-    (check-equal? (input-sequence (list-ref (transaction-inputs tx) 1)) (read-little-endian-hex-string "FFFFFFFF"))
-    ))
-
-(module+ test
-  (test-case
-   "parse transactions with optional input fields not provided"
-   (let ([tx (transaction #:lock-time 10
-                          #:version-number 10
-                          #:flag "g"
-                          #:inputs ((#:witness "w1" #:prevout ("hash1" 1) #:script "op1")
-                                    (#:sequence "EFFFFFFF" #:witness "w2" #:prevout ("hash2" 2) #:script "op2")))])
-     (check-equal? (input-witness (list-ref (transaction-inputs tx) 0)) "w1")
-     (check-equal? (input-sequence (list-ref (transaction-inputs tx) 1)) (read-little-endian-hex-string "EFFFFFFF"))
-     )))
-
-(module+ test
-  (test-case
-   "parse transactions with outputs"
-   (let ([tx (transaction #:lock-time 10
-             #:version-number 10
-             #:flag "g"
-             #:inputs ((#:sequence "FFFFFFFF" #:witness 'w1 #:prevout ("hash1" 1) #:script "op1")
-                       (#:sequence "FFFFFFFF" #:witness 'w2 #:prevout ("hash2" 2) #:script "op2"))
-             #:outputs ((#:script "aa" #:amount 100) (#:script "bb" #:amount 200)))])
-     (check-equal? (length (transaction-outputs tx)) 2)
-     (check-equal? (output-script (list-ref (transaction-outputs tx) 0)) "aa")
-     (check-equal? (output-value (list-ref (transaction-outputs tx) 0)) 100)
-     (check-equal? (output-script (list-ref (transaction-outputs tx) 1)) "bb")
-     (check-equal? (output-value (list-ref (transaction-outputs tx) 1)) 200))))
-
-(module+ test
-  (test-case
-   "transaction with nothing but an output"
-   (let ([tx (transaction #:outputs ((#:script "zz" #:amount 200.20)))])
-     (check-equal? (length (transaction-outputs tx)) 1)
-     (check-equal? (output-script (list-ref (transaction-outputs tx) 0)) "zz")
-     (check-equal? (output-value (list-ref (transaction-outputs tx) 0)) 200.20))))
-     
